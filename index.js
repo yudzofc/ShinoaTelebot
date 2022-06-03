b8465327fb55e9e670831b09fe68e9ae0956fb6d
boedzhanks <hardiansyahramadhani084@gmail.com>
2022-06-04 00:42:33 +0700

    update

--- index.js
+++ index.js
@@ -892,12 +892,21 @@ bot.on("message", async (lol) => {
         await lol.replyWithPhoto({ url: result.result })
         break
 		
-	case 'wallpaperanime':
+	case 'wallpaperanime1':
 			if (args.length == 0) return await reply(`Example: ${prefix + command} shinoa`)
 				text = args.join(" ")
 			result = await wall.getAnimeWall4({ title: `${text}`, type: "sfw", page: 1 })
 			bang = result[Math.floor(Math.random() * result.length)]
 			await lol.replyWithPhoto({ url: bang.image })
+        break
+		
+	case 'wallpaperanime2':
+			if (args.length == 0) return await reply(`Example: ${prefix + command} shinoa`)
+				text = args.join(" ")
+			result = await wall.getAnimeWall2({ title: `${text}`, type: "sfw", page: 1 })
+			bang = result[Math.floor(Math.random() * result.length)]
+			tod = `https://wallpapercave.com/${bang}`
+			await lol.replyWithPhoto({ url: tod })
         break
 
       // Textprome //
